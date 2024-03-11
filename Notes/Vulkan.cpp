e5947e15326097261cf3ce4f5335ee398e7d5232
Hakkology <yildizhakan88@gmail.com>
2024-03-11 16:57:23 +0300

    First commit.

---
+++ Notes/Vulkan.cpp
@@ -0,0 +1,15 @@
+/*
+
+To do list:
+
+- Initiate Vulkan and setup GPU for usage.
+- Validation layers to validate Vulkan code.
+- Setup and use of Swapchain (swap buffer functionality)
+- Create pipeline and shaders in SPIR-V
+- Setup a Render Pass and Sub passes
+- Create and use command buffer on queue
+
+
+
+
+*/
