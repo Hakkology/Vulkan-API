6b0b533e560f63e8823e1fa084fd1171680b6361
Hakkology <yildizhakan88@gmail.com>
2024-03-17 06:21:21 +0300

    glm, Vulkan, glfw check.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <glm/glm.hpp>
 #include <glm/mat4x4.hpp>
 
-#include <stdio.h>
+#include <iostream>
 
 
 
@@ -16,4 +16,28 @@ int main (){
     // Setup details about window
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
+
+    // extension properties, not stored atm.
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    glm::mat4 testMatrix(1.0f);
+    glm::vec4 testVector(1.0f);
+
+    auto testResult = testMatrix * testVector;
+
+    // 12 extensions.
+    printf("Extension count: %i\n", extensionCount);
+
+    // To keep window open
+    while(!glfwWindowShouldClose(window)){
+
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
